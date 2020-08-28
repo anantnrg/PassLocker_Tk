fde33a0803217265862c48883bd125b19ff8c0ce
Anant Narayan <47025756+anantnrg@users.noreply.github.com>
2020-08-28 23:28:05 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import os
+import sqlite3
 
 
 class Login(Frame):
@@ -13,6 +16,30 @@ class Login(Frame):
         self.login_window.title("Master Login - PassLocker")
         self.login_window.iconbitmap(r'login.ico')
         self.pack(fill=BOTH, expand=1)
+        passwd_text = "\u2022"
+
+        # Functions
+        def main():
+            os.system('python main.py')
+            self.quit()
+
+        def validate():
+            usr = txtbox_usr.get()
+            passwd = txtbox_passwd.get()
+            if len(txtbox_usr.get()) == 0:
+                messagebox.showerror(title="Error!", message="No User Name provided. Please "
+                                                             "provide a User Name and Password to login to PassLocker")
+                txtbox_usr.focus()
+            else:
+                pass
+
+            if len(txtbox_passwd.get()) == 0:
+                messagebox.showerror(title="Error!", message="No Password provided. Please "
+                                                             "provide a User Name and Password to login to PassLocker")
+                txtbox_passwd.focus()
+            else:
+                pass
+                db_validate()
 
         # Labels
         lbl_usr = ttk.Label(font='Ubuntu', text='User Name')
@@ -23,14 +50,18 @@ class Login(Frame):
         # Textboxes
         txtbox_usr = ttk.Entry(font='Ubuntu')
         txtbox_usr.place(x=150, y=30)
-        txtbox_passwd = ttk.Entry(font='Ubuntu')
+        txtbox_usr.focus()
+        txtbox_passwd = ttk.Entry(font='Ubuntu',show=passwd_text)
         txtbox_passwd.place(x=150, y=70)
 
         # Buttons
-        cancel = ttk.Button(self, text="Cancel")
+        cancel = ttk.Button(self, text="Cancel",command=root.destroy)
         cancel.place(x=90, y=130)
-        login = ttk.Button(self, text="Login")
+        login = ttk.Button(self, text="Login",command=validate)
         login.place(x=220, y=130)
+
+        def db_validate():
+            print("validated")
 
 
 root = Tk()
